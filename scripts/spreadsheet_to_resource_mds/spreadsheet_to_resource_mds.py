67e467b9fac7febc5e1b31d77ddec877d1fba8c1
Gigi <dergigi@pm.me>
2019-06-11 08:34:43 +0200

    Scrape permalink for books

--- scripts/spreadsheet_to_resource_mds/spreadsheet_to_resource_mds.py
+++ scripts/spreadsheet_to_resource_mds/spreadsheet_to_resource_mds.py
@@ -33,6 +33,7 @@ for row in books.get_all_values():
     resource_amazon_url = row[8]
     resource_wikipedia_url = row[9]
     resource_free_url = row[10]
+    resource_permalink = row[12]
     # resource_date = row[5] if row[5] != '' else NO_DATE
     resource_excerpt = get_excerpt_from_page(resource_url)
 
@@ -71,6 +72,7 @@ for row in books.get_all_values():
                 f"amazon_url: {resource_amazon_url}\n"
                 f"wikipedia_url: {resource_wikipedia_url}\n"
                 f"free_url: {resource_free_url}\n"
+                f"permalink: {resource_permalink}\n"
                 f"---")
 
     with open(md_file_path, 'w') as f:
