1409c5ad51b90d8b1f0a6cf70ee4d9a5e35daaea
Gigi <dergigi@pm.me>
2020-10-22 23:36:48 +0200

    fix(scrape): fix script for mac os

--- scrape.sh
+++ scrape.sh
@@ -7,11 +7,11 @@ fi
 
 readability-cli $1 > scraped.html
 pandoc -f html -t markdown scraped.html > markdown.md
-sed -i'' -- 's/<div>//g' markdown.md
-sed -i'' -- 's/<\/div>//g' markdown.md
-sed -i'' -E -- 's/^::+.*$//g' markdown.md
-sed -i'' -E -- 's/\[(.+)\]\{.+\}/\1/g' markdown.md
-sed -i'' -E -- 's/^>(.+) --- (.+)$/\1\n>\n> <cite>\2<\/cite>/g' markdown.md
+sed -i '' -- 's/<div>//g' markdown.md
+sed -i '' -- 's/<\/div>//g' markdown.md
+sed -i '' -E -- 's/^::+.*$//g' markdown.md
+sed -i '' -E -- 's/\[(.+)\]\{.+\}/\1/g' markdown.md
+sed -i '' -E -- 's/^>(.+) --- (.+)$/\1\n>\n> <cite>\2<\/cite>/g' markdown.md
 cat -s markdown.md > markdown-clean.md
 rm -v scraped.html
 rm -v markdown.md
