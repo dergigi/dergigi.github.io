fafca7537df529b00b0cda3268133487526d0a7a
Gigi <dergigi@pm.me>
2022-10-09 21:35:19 +0200

    feat(scripts): download all ways

--- update-ways.sh
+++ update-ways.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
 wget -O ./_includes/way/01.md https://raw.githubusercontent.com/21-ways/21-ways.github.io/master/_includes/way/01.md
-
+wget -O ./_includes/way/02.md https://raw.githubusercontent.com/21-ways/21-ways.github.io/master/_includes/way/02.md
+wget -O ./_includes/way/03.md https://raw.githubusercontent.com/21-ways/21-ways.github.io/master/_includes/way/03.md
+wget -O ./_includes/way/04.md https://raw.githubusercontent.com/21-ways/21-ways.github.io/master/_includes/way/04.md
+wget -O ./_includes/way/05.md https://raw.githubusercontent.com/21-ways/21-ways.github.io/master/_includes/way/05.md
+wget -O ./_includes/way/06.md https://raw.githubusercontent.com/21-ways/21-ways.github.io/master/_includes/way/06.md
+wget -O ./_includes/way/07.md https://raw.githubusercontent.com/21-ways/21-ways.github.io/master/_includes/way/07.md
+wget -O ./_includes/way/08.md https://raw.githubusercontent.com/21-ways/21-ways.github.io/master/_includes/way/08.md
+wget -O ./_includes/way/09.md https://raw.githubusercontent.com/21-ways/21-ways.github.io/master/_includes/way/09.md
+wget -O ./_includes/way/10.md https://raw.githubusercontent.com/21-ways/21-ways.github.io/master/_includes/way/10.md
+wget -O ./_includes/way/11.md https://raw.githubusercontent.com/21-ways/21-ways.github.io/master/_includes/way/11.md
+wget -O ./_includes/way/12.md https://raw.githubusercontent.com/21-ways/21-ways.github.io/master/_includes/way/12.md
+wget -O ./_includes/way/13.md https://raw.githubusercontent.com/21-ways/21-ways.github.io/master/_includes/way/13.md
+wget -O ./_includes/way/14.md https://raw.githubusercontent.com/21-ways/21-ways.github.io/master/_includes/way/14.md
+wget -O ./_includes/way/15.md https://raw.githubusercontent.com/21-ways/21-ways.github.io/master/_includes/way/15.md
+wget -O ./_includes/way/16.md https://raw.githubusercontent.com/21-ways/21-ways.github.io/master/_includes/way/16.md
+wget -O ./_includes/way/17.md https://raw.githubusercontent.com/21-ways/21-ways.github.io/master/_includes/way/17.md
+wget -O ./_includes/way/18.md https://raw.githubusercontent.com/21-ways/21-ways.github.io/master/_includes/way/18.md
+wget -O ./_includes/way/19.md https://raw.githubusercontent.com/21-ways/21-ways.github.io/master/_includes/way/19.md
+wget -O ./_includes/way/20.md https://raw.githubusercontent.com/21-ways/21-ways.github.io/master/_includes/way/20.md
+wget -O ./_includes/way/21.md https://raw.githubusercontent.com/21-ways/21-ways.github.io/master/_includes/way/21.md
