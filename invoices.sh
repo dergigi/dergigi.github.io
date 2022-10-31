26cf016fd3139eb17e80a4ad1ab4db77a8b5beee
Gigi <dergigi@pm.me>
2022-10-31 21:57:23 +0100

    feat(scripts): use sed to get clean invoice id per line

--- invoices.sh
+++ invoices.sh
@@ -5,11 +5,16 @@ TOKEN="$1"
 STATUS="settled"
 LIMIT="2100"
 
-QUERY_STRING="?orderId=$ORDER_ID&status=$STATUS&take=$LIMIT"
+# ?orderId=$ORDER_ID&
+
+QUERY_STRING="?status=$STATUS&take=$LIMIT"
 
 curl -v \
      -H "Content-Type: application/json" \
      -H "Authorization: token $TOKEN" \
      -X GET \
      "$BTCPAY_INSTANCE/api/v1/stores/$STORE_ID/invoices/$QUERY_STRING" \
-> _data/busking.json
+| jq "map(.id)" \
+> _data/settled_invoices.json
+
+sed -i '' 's/\[//g; s/\]//g; s/"//g; s/,//g; s/ //g; /^[[:blank:]]*$/ d' _data/settled_invoices.json
